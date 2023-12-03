561c928e81821ee5e1a0b36719475cdd460ae7a9
Asha jyothi <123146230+AJM167@users.noreply.github.com>
2023-12-02 18:31:17 -0600

    completed of LessonListActivity.java

--- LanguageLearningPro/app/src/main/java/com/example/languagelearningpro/LessonListActivity.java
+++ LanguageLearningPro/app/src/main/java/com/example/languagelearningpro/LessonListActivity.java
@@ -1,7 +1,11 @@
 package com.example.languagelearningpro;
+
 import android.content.Intent;
+
 import android.os.Bundle;
+
 import android.view.View;
+
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
